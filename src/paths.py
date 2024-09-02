a13548eba85a286adc5034da6094ec55b2fe92a1
Mehrdad Lashgari <mehr.lashgari@gmail.com>
2024-09-02 18:47:19 +0100

    added code for paths.py and model registry

--- src/paths.py
+++ src/paths.py
@@ -1,8 +1,16 @@
 from pathlib import Path
 
-def get_path_to_data_file(file_name:str):-> Path:
-    pass
+def get_path_to_data_file(file_name:str)-> Path:
+    path_to_this_file=Path(__file__)
+    parent_directory=path_to_this_file.parent.parent
+    
+    path_to_data=parent_directory/'data'/file_name
+    return path_to_data
+
 
 def get_path_to_model(file_name:str)->Path:
-    pass
+    path_to_this_file=Path(__file__)
+    parent_directory=path_to_this_file.parent.parent
+    path_to_models=parent_directory/'models'/file_name
+    return path_to_models
 
